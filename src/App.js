184475fe73bc3fe266083827c38c066aac9d5496
idan2468 <idans2468@gmail.com>
2020-09-23 23:47:32 +0300

    Added orders route to the app with the Orders components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 class App extends Component {
     render() {
@@ -12,6 +13,7 @@ class App extends Component {
                     <Layout>
                         <Switch>
                             <Route path="/checkout" component={Checkout}/>
+                            <Route path="/orders" component={Orders}/>
                             <Route path={'/'}  exact component={BurgerBuilder}/>
                         </Switch>
                     </Layout>
