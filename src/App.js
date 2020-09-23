8c731c1394826d61baba1dc057faa94b02179962
idan2468 <idans2468@gmail.com>
2020-09-23 15:23:24 +0300

    Implemented react routes to the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, {Component,Fragment} from 'react';
-import Layout from './containers/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import React, {Component, Fragment} from 'react';
+import Layout from './containers/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Checkout from "./containers/Checkout/Checkout";
+
 class App extends Component {
     render() {
         return (
             <Fragment>
-                <Layout>
-                    <BurgerBuilder/>
-                </Layout>
+                <BrowserRouter>
+                    <Layout>
+                        <Switch>
+                            <Route path="/checkout" component={Checkout}/>
+                            <Route path={'/'}  exact component={BurgerBuilder}/>
+                        </Switch>
+                    </Layout>
+                </BrowserRouter>
             </Fragment>
         );
     }
