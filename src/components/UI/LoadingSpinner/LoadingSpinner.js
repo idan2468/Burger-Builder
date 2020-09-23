96a1bba0592a2712450458ed8639a47a709697c4
idan2468 <idans2468@gmail.com>
2020-09-23 15:28:44 +0300

    Fixed bug of the spinner trying to update it's state after it's unmounted due to the use of timeout. Added useEffect with unmount func that clears the timeout in case it didn't executed yet

--- src/components/UI/LoadingSpinner/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner/LoadingSpinner.js
@@ -1,17 +1,21 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import styles from './LoadingSpinner.scss';
 
 const loadingSpinner = (props) => {
     const [content, setContent] = useState(null);
-    setTimeout(() => {
-        setContent(
-            <div className={styles.container}>
-                <div className={styles.loader}>
-                    {/*<span>Loading...</span>*/}
+    useEffect(() => {
+        let timeout = setTimeout(() => {
+            setContent(
+                <div className={styles.container}>
+                    <div className={styles.loader}>
+                        {/*<span>Loading...</span>*/}
+                    </div>
                 </div>
-            </div>
-        )
-    }, 500);
+            )
+        }, 500)
+        return () => clearTimeout(timeout);
+    }, [content]);
+
     return (
         <Fragment>
             {content}
