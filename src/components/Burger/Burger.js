86150db9ee3220f9f25d14c7fb8254723ef39cb7
idan2468 <idans2468@gmail.com>
2020-09-23 15:29:20 +0300

    Added hoc withRouter to the Burger component to get the Route props

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,21 +2,25 @@ import PropTypes from 'prop-types'
 import React, {Fragment} from 'react';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 import styles from './Burger.scss';
+import {withRouter} from "react-router";
 
 const burger = (props) => {
+    let ingredientsArray = null;
     // adding visualised burger
-    let ingredientsArray = Object.keys(props.ingredients).map((key, i) => {
-        const ingredientsElements = [];
-        for (let j = 0; j < props.ingredients[key]; j++) {
-            ingredientsElements.push(<BurgerIngredients type={key} key={key + j}/>);
+    if (props.ingredients) {
+        ingredientsArray = Object.keys(props.ingredients).map((key, i) => {
+            const ingredientsElements = [];
+            for (let j = 0; j < props.ingredients[key]; j++) {
+                ingredientsElements.push(<BurgerIngredients type={key} key={key + j}/>);
+            }
+            return ingredientsElements;
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, [])
+        // no ingredients chosen yet
+        if (ingredientsArray.length === 0) {
+            ingredientsArray = <p>Please start to add ingredients!!</p>
         }
-        return ingredientsElements;
-    }).reduce((arr, el) => {
-        return arr.concat(el);
-    }, [])
-    // no ingredients chosen yet
-    if (ingredientsArray.length === 0) {
-        ingredientsArray = <p>Please start to add ingredients!!</p>
     }
     return (
         <Fragment>
@@ -33,4 +37,4 @@ burger.propTypes = {
     ingredients: PropTypes.object
 }
 
-export default burger;
+export default withRouter(burger);
