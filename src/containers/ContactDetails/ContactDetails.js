1fbbd3a83e519777f831c810bf2ee690f6b60f53
IdanS <idans2468@gmail.com>
2020-09-23 18:25:08 +0300

    Updated customer in the server request to save order in the db

--- src/containers/ContactDetails/ContactDetails.js
+++ src/containers/ContactDetails/ContactDetails.js
@@ -21,11 +21,13 @@ class ContactDetails extends Component {
             setTimeout(() => this.state.ordered ? this.setState({loading: true}) : null, 500);
             await axios.put('/order', {
                 price: this.props.price,
-                ingredients: this.props.ingredients
+                ingredients: this.props.ingredients,
+                customer: this.state
             })
             // await new Promise(resolve => setTimeout(() => resolve(), 2000)); delay for showing the spinner
             setTimeout(() => this.setState({loading: false}), 1000)
-        } catch (error) {
+        }
+        catch (error) {
 
         }
         this.props.history.replace('/');
@@ -34,31 +36,31 @@ class ContactDetails extends Component {
 
     render() {
         return (
-            <Fragment>
-                <form className={styles.formContainer}>
-                    <h2>Enter your credentials:</h2>
-                    <label className={styles.formItem}>Full Name
-                        <input type="text" name="fullName" placeholder={'Enter your name'}/>
-                    </label>
-                    <label className={styles.formItem}>Email
-                        <input type="text" name="email" placeholder={'Enter your email'}/>
-                    </label>
-                    <label className={styles.formItem}>Pay Method
-                        <input type="text" name="payMethod" placeholder={'Enter pay method'}/>
-                    </label>
-                    <h4>Address</h4>
-                    <label className={styles.formItem}>City
-                        <input type="text" name="city" placeholder={'Enter City'}/>
-                    </label>
-                    <label className={styles.formItem}>Street
-                        <input type="text" name="street" placeholder={'Enter Streed'}/>
-                    </label>
-                    <label className={styles.formItem}>Postal Code
-                        <input type="text" name="postalCode" placeholder={'Enter Postal Code'}/>
-                    </label>
-                    <Button type={'ok'} text={'Confirm'} onClick={this.submitHandler} className={styles.buttonConfirm}/>
-                </form>
-            </Fragment>
+          <Fragment>
+              <form className={styles.formContainer}>
+                  <h2>Enter your credentials:</h2>
+                  <label className={styles.formItem}>Full Name
+                      <input type="text" name="fullName" placeholder={'Enter your name'}/>
+                  </label>
+                  <label className={styles.formItem}>Email
+                      <input type="text" name="email" placeholder={'Enter your email'}/>
+                  </label>
+                  <label className={styles.formItem}>Pay Method
+                      <input type="text" name="payMethod" placeholder={'Enter pay method'}/>
+                  </label>
+                  <h4>Address</h4>
+                  <label className={styles.formItem}>City
+                      <input type="text" name="city" placeholder={'Enter City'}/>
+                  </label>
+                  <label className={styles.formItem}>Street
+                      <input type="text" name="street" placeholder={'Enter Streed'}/>
+                  </label>
+                  <label className={styles.formItem}>Postal Code
+                      <input type="text" name="postalCode" placeholder={'Enter Postal Code'}/>
+                  </label>
+                  <Button type={'ok'} text={'Confirm'} onClick={this.submitHandler} className={styles.buttonConfirm}/>
+              </form>
+          </Fragment>
         )
     }
 }
