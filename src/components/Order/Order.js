6c0ff7fe14f65833e3620299c1168c20d892c98f
idan2468 <idans2468@gmail.com>
2020-09-23 23:45:04 +0300

    Created Order card component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import React, {Fragment} from 'react';
+import styles from './Order.scss';
+
+const order = (props) => {
+    let ingredients = null;
+    if (props.ingredients) {
+        ingredients = Object.keys(props.ingredients).map(key => {
+            return (
+                <div className={styles.ingredient}>
+                    <h4>{key}</h4>
+                    <h4>{props.ingredients[key]}</h4>
+                </div>
+            )
+        })
+    }
+    return (
+        <Fragment>
+            <div className={styles.container} style={props.style}>
+                <div>
+                    <h3>Name: {props.name}</h3>
+                    <div className={styles.ingredientsContainer}>
+                        <h3>Ingredients:</h3>
+                        {ingredients}
+                    </div>
+                </div>
+                <h3>Price: {props.price}</h3>
+            </div>
+        </Fragment>
+    )
+}
+
+export default order;
+
+order.propTypes = {
+    ingredients: PropTypes.object,
+    name: PropTypes.string,
+    price: PropTypes.number,
+    style: PropTypes.object
+}
