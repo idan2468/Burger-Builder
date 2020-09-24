e7cf795bb5cca3f8a46f8616529ec55eea92f5d5
idan2468 <idans2468@gmail.com>
2020-09-25 02:19:53 +0300

    Generate different inputTypes

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,10 +3,29 @@ import React, {Fragment} from 'react';
 import styles from './Input.scss';
 
 const input = (props) => {
+    let inputElement = null;
+    let optionsElements = props.options.map(option => <option key={option} value={option}>{option}</option>);
+    switch (props.inputType) {
+        case 'input':
+            inputElement =
+                <input type={props.type} name={props.name} placeholder={props.placeholder} value={props.value}
+                       onChange={props.onChange}/>;
+            break
+        case 'select':
+            inputElement = (
+                <select name={props.name} placeholder={props.placeholder} value={props.value}
+                        onChange={props.onChange}>
+                    {optionsElements}
+                </select>
+            );
+    }
     return (
         <Fragment>
             <label className={styles.formItem}>{props.label}
-                <input type={props.type} name={props.name} placeholder={props.placeholder}/>
+                {/*<input type={props.type} name={props.name} placeholder={props.placeholder} value={props.value}*/}
+                {/*       onChange={props.onChange}/>*/}
+                {/*<input {...props}/>*/}
+                {inputElement}
             </label>
         </Fragment>
     )
@@ -17,10 +36,16 @@ export default input;
 input.propTypes = {
     label: PropTypes.string,
     name: PropTypes.string,
+    onChange: PropTypes.func,
     placeholder: PropTypes.string,
-    type: PropTypes.string
+    type: PropTypes.string,
+    value: PropTypes.string,
+    inputType: PropTypes.string,
+    options: PropTypes.array
 }
 
 input.defaultProps = {
-    type: 'text'
+    type: 'text',
+    inputType: 'input',
+    options: []
 }
