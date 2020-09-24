f16c21705b5263eaf90bcd0e2ac10ae23fc0559c
IdanS <idans2468@gmail.com>
2020-09-25 00:55:38 +0300

    Using NavigationItems instead of few NavigationItem components

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,6 +3,7 @@ import React, {Fragment} from 'react';
 import styles from './SideDrawer.scss'
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const sideDrawer = (props) => {
     let sliderStyles = [styles.slider];
@@ -15,8 +16,9 @@ const sideDrawer = (props) => {
         <Fragment>
             <Backdrop show={props.show} onClick={props.onClick}/>
             <div className={sliderStyles.join(' ')}>
-                <NavigationItem link={'/'}>Main</NavigationItem>
-                <NavigationItem link={'/orders'}>Orders</NavigationItem>
+                {/*<NavigationItem link={'/'}>Main</NavigationItem>*/}
+                {/*<NavigationItem link={'/orders'}>Orders</NavigationItem>*/}
+                <NavigationItems side/>
             </div>
         </Fragment>
     )
