2afb7b0690b2dc2048d15c177856220a9391c15a
idan2468 <idans2468@gmail.com>
2020-09-25 02:15:16 +0300

    Removed comments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types'
 import React, {Fragment} from 'react';
 import styles from './SideDrawer.scss'
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 const sideDrawer = (props) => {
@@ -16,8 +15,6 @@ const sideDrawer = (props) => {
         <Fragment>
             <Backdrop show={props.show} onClick={props.onClick}/>
             <div className={sliderStyles.join(' ')}>
-                {/*<NavigationItem link={'/'}>Main</NavigationItem>*/}
-                {/*<NavigationItem link={'/orders'}>Orders</NavigationItem>*/}
                 <NavigationItems side/>
             </div>
         </Fragment>
