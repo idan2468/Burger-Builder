21962ab7a8625407283d682cca09a74551ec1169
idan2468 <idans2468@gmail.com>
2020-09-26 16:37:40 +0300

    Continue implement redux with implementing actions and dispatchers.

---
+++ src/store/actions.js
@@ -0,0 +1,1 @@
+export const CHANGE_ING_COUNT = 'CHANGE_ING_COUNT';
--- src/store/reducers/burgerReducer.js
+++ src/store/reducers/burgerReducer.js
@@ -1,11 +1,36 @@
+import * as actions from '../actions';
+
+const ingredientsPrices = {
+    'cheese': 1,
+    'salad': 2,
+    'bacon': 3,
+    'meat': 4,
+}
+
 const initialState = {
-    ingredients: {},
+    ingredients: {
+        'cheese': 0,
+        'salad': 0,
+        'bacon': 0,
+        'meat': 0,
+    },
     price: 0
 }
 
 
 const burgerReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case actions.CHANGE_ING_COUNT:
+            let itemChange = action.value - state.ingredients[action.ingredient];
+            let newPrice = state.price + itemChange * ingredientsPrices[action.ingredient];
+            return {
+                ...state,
+                ingredients: {...state.ingredients, [action.ingredient]: action.value},
+                price: newPrice
+            }
+        default:
+            return state;
+    }
 }
 
 export default burgerReducer;
