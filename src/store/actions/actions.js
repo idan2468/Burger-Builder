693603807bb3eb2d7e27961fce0cb69b5bb916be
idan2468 <idans2468@gmail.com>
2020-10-02 15:04:02 +0300

    Added register handler to the store

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,4 +1,4 @@
 export * from './burgerActions';
 // using redux tool kit
 export {fetchOrders, createOrder} from '../slices/ordersReducer';
-export {loginHandler} from '../slices/authReducer';
+export {loginHandler, registerHandler} from '../slices/authReducer';
--- src/store/slices/authReducer.js
+++ src/store/slices/authReducer.js
@@ -12,6 +12,17 @@ export const loginHandler = createAsyncThunk('LOGIN_HANDLER',
             return rejectWithValue(e.message);
         }
     });
+export const registerHandler = createAsyncThunk('REGISTER_HANDLER',
+    async ({username, password, email}, {rejectWithValue}) => {
+        try {
+            // await new Promise(resolve => setTimeout(() => resolve(), 2000));
+            const res = await axios.post('/register', {username: username, password: password, email: email});
+            console.dir(res);
+            return res.data.token;
+        } catch (e) {
+            return rejectWithValue(e.message);
+        }
+    });
 
 const initialState = {
     token: null,
@@ -33,6 +44,17 @@ const login = createSlice({
             state.loading = false;
         },
         [loginHandler.rejected]: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        },
+        [registerHandler.pending]: (state) => {
+            state.loading = true;
+        },
+        [registerHandler.fulfilled]: (state, action) => {
+            state.token = action.payload;
+            state.loading = false;
+        },
+        [registerHandler.rejected]: (state, action) => {
             state.error = action.payload;
             state.loading = false;
         },
